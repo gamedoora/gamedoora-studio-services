853a9ed0287deb179847f02399057bda450922f8
Anan <ananshah24@gmail.com>
2023-08-27 21:31:00 +0530

    added comments and extented one IsCommunity

--- src/test/java/com/gamedoora/backend/studioservices/repositoryTest.java
+++ src/test/java/com/gamedoora/backend/studioservices/repositoryTest.java
@@ -20,21 +20,23 @@ import org.springframework.test.context.TestPropertySource;
 
 @DataJpaTest(properties="spring.cloud.config.enabled=false")
 @TestPropertySource("classpath:test.properties")
-@EntityScan("com.gamedoora.model.*")
+@EntityScan("com.gamedoora.model.*")	// Scanning the model(s)
 class repositoryTest {
 
 	@Autowired
 	private StudioRepository repo;
 
-	@Autowired
-	private StudioRepository studioRepository;
 
 	@Autowired
 	private TestEntityManager entityManager;
 
-	
+	// Can Use @BeforeEach annotation
+
+
 	@Test
 	public void test() {
+		// Checking object creation and its storing
+
 		Studios studio = Studios.builder().id(123).name("Yoshita").build();
 
 		Studios savedStudio = repo.save(studio);
@@ -46,34 +48,57 @@ class repositoryTest {
 
 	@Test
 	public void findByIsCommunity(){
-		Studios studio1 = Studios.builder().id(1).name("A").isCommunity(1).description("DA").build();
-		Studios studio2 = Studios.builder().id(2).name("b").isCommunity(2).description("DA").visibility(false).build();
+		// single checking for IsCommunity
 
+		// To use for IsCommunity - 0 or 1 as True - False
+
+		Studios studio1 = Studios.builder().id(1).name("A").isCommunity(1).description("DA").build();
+		Studios studio2 = Studios.builder().id(2).name("b").isCommunity(0).description("DA").visibility(false).build();
+		Studios studio3 = Studios.builder().id(3).name("b").isCommunity(1).description("DA").visibility(false).build();
+
+		// check 1
 		repo.save(studio1);
 		List<Studios> commu = repo.findByIsCommunity(1);
 		Assertions.assertEquals(1,commu.size());
+
+		Assertions.assertEquals("A",commu.get(0).getName());
+
+		// check 1 Extended
+		repo.save(studio2);
+		repo.save(studio3);
+		//repo.save(studio2);	// sequence changing affects below assertions
+
+		commu = repo.findByIsCommunity(1);
+		Assertions.assertEquals(2,commu.size());
+		Assertions.assertEquals("A",commu.get(0).getName());
+		Assertions.assertEquals("b",commu.get(1).getName());
 	}
 
 	@Test
 	public void testFindByIsCommunity() {
 		// Create some test studios with different is_community values
 
-		// IsCommunity takes only binary values - 0 or 1
+		// To use for IsCommunity - 0 or 1 as True - False
 
 		Studios studio1 = Studios.builder().id(1).name("A").isCommunity(1).description("DA").build();
-		Studios studio2 = Studios.builder().id(2).name("A").isCommunity(1).description("DA").visibility(false).build();
+		Studios studio2 = Studios.builder().id(2).name("A").isCommunity(0).description("DA").visibility(false).build();
 
 		List<Studios> list = List.of(studio1,studio2);
 		repo.saveAll(list);
 		// Perform the repository method call
 		List<Studios> communityStudios = repo.findByIsCommunity(1);
 
-		assertEquals(2,communityStudios.size());
+		assertEquals(1,communityStudios.size());
 		for(int i=0; i<communityStudios.size();i++)
-		assertEquals("A",(communityStudios.get(i)).getName());
+		assertEquals("A",(communityStudios.get(i)).getName()); // works well with for loops
+
 		//assertThat(communityStudios).hasSize(1).contains(studio1);  // Uses ArrayList
 
 	}
+
+	// To use TestEntityManager instead of EntityManager for testing environment
+
+	// Below Shows detached state error for TestEntityManager.persist(_object_)
 
 //	@Test
 //	public void findByUsersSet_Id()
